ad0816dc89192f33c20dec014637abc8b45816b2
Anna353 <anechka353you@gmail.com>
2022-07-04 15:16:44 +0300

    homework

--- Start_Selenium/src/test/java/Start.java
+++ Start_Selenium/src/test/java/Start.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,33 +25,59 @@ public class Start {
         wd.quit();
     }
  @Test
-  public void firstTest(){
+  public void firstTestLoginSuccess(){
 
         wd = new ChromeDriver();
         wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
 
-//        //open form -> click login button
-//       WebElement loginTab;
-//       loginTab.click();
-//
-//       //fill valid email
-//       WebElement emailTextBox;
-//       emailTextBox.click();
-//       emailTextBox.clear();
-//       emailTextBox.sendKeys("test@gmail.com);
-//
-//      // fill valid password
-//       WebElement passwordTextBox;
-//       passwordTextBox.click();
-//       passwordTextBox.clear();
-//       passwordTextBox.sendKeys("Tt12345$");
-//
-//       //click login button
-//       WebElement loginButton;
-//       loginButton.click();
-//
-//       //Assert
+        //open form -> click login button
+       WebElement loginTab=wd.findElement(By.cssSelector("[href='/login']"));
+       loginTab.click();
+
+       //fill valid email
+       WebElement emailTextBox= wd.findElement(By.cssSelector("[placeholder='Email']"));
+       emailTextBox.click();
+       emailTextBox.clear();
+       emailTextBox.sendKeys("noa@gmail.com");
+
+      // fill valid password
+       WebElement passwordTextBox= wd.findElement(By.cssSelector("[placeholder='Password']"));
+       passwordTextBox.click();
+       passwordTextBox.clear();
+       passwordTextBox.sendKeys("Nnoa12345$");
+
+       //click login button
+       WebElement loginButton= wd.findElement(By.cssSelector("button"));
+       loginButton.click();
+
+       //Assert
 
    }
 
+
+    @Test
+    public void firstTestRegistrSuccess(){
+        wd = new ChromeDriver();
+        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
+          WebElement loginTab =wd.findElement(By.cssSelector("[href='/login']"));
+          loginTab.click();
+
+        WebElement emailTextBox= wd.findElement(By.cssSelector("[placeholder='Email']"));
+        emailTextBox.click();
+        emailTextBox.clear();
+        emailTextBox.sendKeys("Knna@gmail.com");
+
+
+        WebElement passwordTextBox= wd.findElement(By.cssSelector("[placeholder='Password']"));
+        passwordTextBox.click();
+        passwordTextBox.clear();
+        passwordTextBox.sendKeys("Aanna12345$");
+
+        WebElement registrButton= wd.findElement(By.cssSelector("div.login_login__3EHKB button:last-child"));
+        registrButton.click();
+
+
+
+
+    }
 }
--- Start_Selenium/src/test/java/StartCss.java
+++ Start_Selenium/src/test/java/StartCss.java
@@ -10,21 +10,21 @@ public class StartCss {
     WebDriver wd;
 
     @Test
-    public void startCssLocators(){
+    public void startCssLocators() {
         wd = new ChromeDriver();
         wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
-      WebElement el1 = wd.findElement(By.id("root"));
-      WebElement el2 = wd.findElement(By.tagName("a"));
-      List<WebElement> list = wd.findElements(By.tagName("a"));  //-> 3a
-      wd.findElement(By.className("container"));
-     // wd.findElement(By.name("name"));
+        WebElement el1 = wd.findElement(By.id("root"));
+        WebElement el2 = wd.findElement(By.tagName("a"));
+        List<WebElement> list = wd.findElements(By.tagName("a"));  //-> 3a
+        wd.findElement(By.className("container"));
+        // wd.findElement(By.name("name"));
 
-      wd.findElement(By.linkText("LOGIN"));
-      wd.findElement(By.partialLinkText("LOG"));
+        wd.findElement(By.linkText("LOGIN"));
+        wd.findElement(By.partialLinkText("LOG"));
 
-   /////////////////////CSS//////////////
+        /////////////////////CSS//////////////
 //tagName
-    wd.findElement(By.cssSelector("div"));
+        wd.findElement(By.cssSelector("div"));
         wd.findElement(By.cssSelector("a"));
         wd.findElement(By.cssSelector("input"));
         wd.findElement(By.cssSelector("button"));
@@ -33,7 +33,6 @@ public class StartCss {
 //class ---> .class
         wd.findElement(By.cssSelector(".container"));
         wd.findElement(By.cssSelector("[href='/home']"));
-
 
 
     }
